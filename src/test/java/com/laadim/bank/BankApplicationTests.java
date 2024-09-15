bbc39bde99670fada3ac004638de51c0e866f796
laadiim <112978764+laadiim@users.noreply.github.com>
2024-09-15 11:38:31 +0200

    ver: 0.0.0 - load

---
+++ src/test/java/com/laadim/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laadim.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
